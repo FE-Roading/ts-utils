30c4e2ed7cd26a3db2ef7d0cfc380a999b2cef7e
qixm <qixm@burnish.cn>
2021-06-02 11:38:57 +0800

    project init

---
+++ types/index.d.ts
@@ -0,0 +1,11 @@
+declare interface Fn<T = any, R = T> {
+  (...arg: T[]): R;
+}
+
+declare interface PromiseFn<T = any, R = T> {
+  (...arg: T[]): Promise<R>;
+}
+
+declare type TargetContext = '_self' | '_blank';
+
+declare type ElRef<T extends HTMLElement = HTMLDivElement> = Nullable<T>;
